51ee12be7f95150e1ef5053cf57f62478ff58e1c
Eimear Foley <eimearfoley96@gmail.com>
2016-09-28 15:38:34 +0100

    Update lab1.py

--- CS2515/lab1.py
+++ CS2515/lab1.py
@@ -1,3 +1,5 @@
+"""Introduction to designing, implementing and testing algorithms and data structures in Python."""
+
 from time import perf_counter
 import random
 
@@ -54,6 +56,9 @@ def countMethod(lst1,lst2):
     return True
 
 def performanceCheck(lst1,lst2):
+    
+    """Performance check function which will take two lists as input, and write out to the screen the runtime 
+    required for each of the three functions above to check whether the inputs are anagrams of each other. """
 
     print('Length of list 1: %i' %(len(lst1)))
     print('Length of list 2: %i' %(len(lst2)))
@@ -75,6 +80,8 @@ def performanceCheck(lst1,lst2):
         print('Count Method: %f seconds' % (after_time - before_time))
 
 def randomListsCheck(n):
+    
+    """Function to compare the performance of the three functions on randomly generated lists of a given length."""
 
     lst = []
     for i in range(n):
@@ -84,6 +91,21 @@ def randomListsCheck(n):
     performanceCheck(lst, random_copy)
 
 def countDict(lst1,lst2):
-
-    d = {n : 0 for n in range(10000)}
+    
+    """Adapted countMethod to use a dictionary, where the elements of the input lists are the keys, 
+    and the difference in the number of times they appear in the two input lists are the values. 
+    
+    Any key that is not in the dictionary must have the value 0."""
+    
+    if len(lst1) != len(lst2):
+        return False
+    
+    d = {i : 0 for i in lst1}
+    
+    for i in range(len(lst1)):
+        d[lst2[i]] += 1
+    for n in range(len(d)):
+        if d[n] == 0:
+            return False
+    return True
     
