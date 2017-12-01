6b1526b95b11a10e3c1881f75d1a828460557e13
Eimear Foley <eimearfoley96@gmail.com>
2017-12-01 16:20:59 +0000

    Create UnivariateFunction.java

---
+++ Year 3/Java/UnivariateFunction.java
@@ -0,0 +1,18 @@
+package A_01;
+
+/**
+ * Univariate Function Interface
+ * @author Eimear Foley
+ * @version 1 (27th November 2017)
+ */
+public interface UnivariateFunction {
+
+    /**
+     * Specification for a univariate function
+     * @return double
+     */
+    static double f() {
+        return f();
+    }
+
+}
